b13802db51afbf9c12fb1435045e20c05c13cc88
Chris Wingard <wingardc@nevada.unr.edu>
2023-12-01 15:21:04 -0500

    Add day 1 puzzle 2

--- 2023/day1/puzzle1.py
+++ 2023/day1/puzzle1.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python3
 
-# Fairly straightforward brute force approach.
+
+import sys
 
 
 def get_first_number(text):
-    print("Checking '", text, "'", sep="")
-    
     for c in text:
         if c.isdigit():
             return c
@@ -13,9 +12,9 @@ def get_first_number(text):
     # No digit found.  This is not expected.
     print("No digit found in: '", text, "'", sep="")
     raise Exception
-        
 
-def main():
+
+def puzzle1():
     f = open("input.txt")
     lines = f.read().split("\n")
 
@@ -26,6 +25,61 @@ def main():
             the_sum += num
 
     print(the_sum)
+
+
+def get_earliest_digit(text, digit_map):
+    earliest = sys.maxsize
+    the_earliest = ""
+
+    for key in digit_map:
+        index = text.find(key)
+        if index < earliest and index >= 0:
+            earliest = index
+            the_earliest = digit_map[key]
+
+    return the_earliest
+
+
+def puzzle2():
+    digits = {
+        "one"   : "1",
+        "two"   : "2",
+        "three" : "3",
+        "four"  : "4",
+        "five"  : "5",
+        "six"   : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine"  : "9",
+        "1"     : "1",
+        "2"     : "2",
+        "3"     : "3",
+        "4"     : "4",
+        "5"     : "5",
+        "6"     : "6",
+        "7"     : "7",
+        "8"     : "8",
+        "9"     : "9"
+    }
+    digits_keys_backwards = {}
+    for k,v in digits.items():
+        digits_keys_backwards[k[::-1]] = v
+
+    f = open("input.txt")
+    lines = f.read().split("\n")
+
+    the_sum = 0
+    for line in lines:
+        if len(line) > 0:
+            num = int(get_earliest_digit(line, digits) + get_earliest_digit(line[::-1], digits_keys_backwards))
+            the_sum += num
+
+    print(the_sum)
+
+
+def main():
+    puzzle1()
+    puzzle2()
 
 
 if __name__ == "__main__":
